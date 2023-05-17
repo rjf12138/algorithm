34e55048b93f9075eec4884fe9c00daeaedd9f70
rjf12138 <3161173206@qq.com>
2023-05-17 22:41:17 +0800

    2023年 05月 17日 星期三 22:41:17 CST

--- inc/algorithm.h
+++ inc/algorithm.h
@@ -17,6 +17,8 @@ namespace algorithm
     void murmurhash3_x86_32(const void *key, int len, uint32_t seed, void *out );
     void murmurhash3_x86_128(const void *key, int len, uint32_t seed, void *out );
     void murmurhash3_x64_128(const void *key, int len, uint32_t seed, void *out );
+
+    // CRC 校验
 } // namespace algorithm
 
 
